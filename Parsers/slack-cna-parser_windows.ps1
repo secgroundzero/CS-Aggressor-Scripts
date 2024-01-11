f282674dbe24d5ae0acc1564f49b63ac8a5959f2
Nikos Vourdas <32869078+nickvourd@users.noreply.github.com>
2024-01-11 13:34:17 +0200

    Create slack-cna-parser_windows.ps1

---
+++ Parsers/slack-cna-parser_windows.ps1
@@ -0,0 +1,18 @@
+$global:version = "1.0"
+
+$ascii = @"
+
+ ██████ ███    ██  █████        ██████   █████  ██████  ███████ ███████ ██████  
+██      ████   ██ ██   ██       ██   ██ ██   ██ ██   ██ ██      ██      ██   ██ 
+██      ██ ██  ██ ███████ █████ ██████  ███████ ██████  ███████ █████   ██████  
+██      ██  ██ ██ ██   ██       ██      ██   ██ ██   ██      ██ ██      ██   ██ 
+ ██████ ██   ████ ██   ██       ██      ██   ██ ██   ██ ███████ ███████ ██   ██ 																		       
+  
+~ Created with <3 by @nickvourd
+~ Special thanks to @sec_groundzero
+~ Version: $global:version
+~ Type: Slack CNA Parser Tool (Windows Edition)
+
+"@
+
+Write-Host $ascii`n
